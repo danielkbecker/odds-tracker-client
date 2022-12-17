b1acafc8234a48fe6b30ec35cf952961ee71c538
pythonscrapers <118010816+pythonscrapers@users.noreply.github.com>
2022-12-17 10:12:37 -0500

    Added e2e testing with Cypress. Added an e2e test that does login flow.

---
+++ .eslintrc.js
@@ -0,0 +1,16 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true,
+  },
+  extends: [
+    'plugin:vue/vue3-essential',
+    '@vue/airbnb',
+  ],
+  parserOptions: {
+    parser: '@babel/eslint-parser',
+  },
+  rules: {
+    'no-console': 'off',
+  },
+};
