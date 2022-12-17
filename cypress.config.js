c9e882c2ecb516eb339669e3312de68dc22d0c4c
pythonscrapers <118010816+pythonscrapers@users.noreply.github.com>
2022-12-17 11:54:38 -0500

    Updated Cypress testing to include environment variables in a hidden location. Changed the name of e2e testing file to authflow, created a cypress config file.

---
+++ cypress.config.js
@@ -0,0 +1,27 @@
+const { defineConfig } = require('cypress');
+const setupNodeEvents = require('./tests/e2e/plugins/index');
+
+module.exports = defineConfig({
+  fixturesFolder: 'tests/e2e/fixtures',
+  screenshotsFolder: 'tests/e2e/screenshots',
+  videosFolder: 'tests/e2e/videos',
+
+  e2e: {
+    // We've imported your old cypress plugins here.
+    // You may want to clean this up later by importing these.
+    setupNodeEvents() {
+      // // eslint-disable-next-line import/extensions,global-require
+      // return require('./tests/e2e/plugins/index.js')(on, config);
+    },
+    specPattern: 'tests/e2e/specs/**/*.cy.{js,jsx,ts,tsx}',
+    supportFile: 'tests/e2e/support/index.js',
+    testIsolation: false,
+  },
+  component: {
+    devServer: {
+      framework: 'vue-cli',
+      bundler: 'webpack',
+    },
+    setupNodeEvents,
+  },
+});
