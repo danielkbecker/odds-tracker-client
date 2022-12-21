298b96b2895982d63d46fba5b5e21e6683d008e0
pythonscrapers <118010816+pythonscrapers@users.noreply.github.com>
2022-12-21 11:48:43 -0500

    Replaced flowbite avatar dropdown component with a better native tailwind option that doesnt use flowbite.

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -8,40 +8,35 @@
     <div class="flex w-1/3 justify-center">
       <img class="image max-h-16" src="../assets/logo.png" alt="foo">
     </div>
-    <div class="flex w-1/3 justify-end">
-    <div class="flex justify-end mr-10">
-        <Dropdown class="cursor-pointer" text="Left" placement="left" v-if="isLoggedIn">
-          <template #trigger>
-            <span><img class="image max-h-8" src="../assets/accounticon.png" alt="foo"></span>
-          </template>
-          <ListGroup>
-            <list-group-item>
-              <template #prefix>
-                <svg class="w-5 h-5 ml-0 fill-current" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M5 4a1 1 0 00-2 0v7.268a2 2 0 000 3.464V16a1 1 0 102 0v-1.268a2 2 0 000-3.464V4zM11 4a1 1 0 10-2 0v1.268a2 2 0 000 3.464V16a1 1 0 102 0V8.732a2 2 0 000-3.464V4zM16 3a1 1 0 011 1v7.268a2 2 0 010 3.464V16a1 1 0 11-2 0v-1.268a2 2 0 010-3.464V4a1 1 0 011-1z"></path></svg>
-              </template>
-              Account
-            </list-group-item>
-            <ListGroupItem @click="logoutFromApp">
-              <template #prefix >
-                <img class="max-h-5 max-w-sm ml-.75" src="../assets/logout.png" alt="foo">
-                <!--<svg class="w-4 h-4 fill-current" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-6-3a2 2 0 11-4 0 2 2 0 014 0zm-2 4a5 5 0 00-4.546 2.916A5.986 5.986 0 0010 16a5.986 5.986 0 004.546-2.084A5 5 0 0010 11z" clip-rule="evenodd"></path></svg>-->
-              </template>
-              Logout
-            </ListGroupItem>
-          </ListGroup>
-        </Dropdown>
-    </div>
+    <div class="flex w-1/3 justify-end items-center">
+      <VDropdown placement="right" v-if="isLoggedIn" class="mr-4">
+    <!-- Button content -->
+    <template v-slot:button>
+      <img class="w-10 h-10 rounded-full border-gray-600 mr-3" src="../assets/accounticon.png" alt="Saleh Mir" />
+    </template>
+
+    <!-- Opened dropdown content -->
+    <template v-slot:content>
+      <a class="flex w-full justify-between items-center rounded px-2 py-1 my-1 hover:bg-indigo-600 hover:text-white" href="#">Profile</a>
+      <a class="flex w-full justify-between items-center rounded px-2 py-1 my-1 hover:bg-indigo-600 hover:text-white" href="#">Settings</a>
+      <a class="flex w-full justify-between items-center rounded px-2 py-1 my-1 hover:bg-indigo-600 hover:text-white" href="#">Help</a>
+
+      <hr />
+
+      <a class="flex w-full justify-between items-center rounded px-2 py-1 my-1 hover:bg-red-600 hover:text-white" href="#" @click.prevent="logoutFromApp">Logout</a>
+    </template>
+  </VDropdown>
     </div>
   </nav>
 </template>
 
 <script>
-import { Dropdown, ListGroup, ListGroupItem } from 'flowbite-vue';
 import { mapActions, mapState } from 'pinia';
+import VDropdown from './VDropdown.vue';
 import { useAuthStore } from '../stores/AuthStore';
 
 export default {
-  components: { Dropdown, ListGroupItem, ListGroup },
+  components: { VDropdown },
   data() {
     return {
       dropdownAvatarName: false,
@@ -57,9 +52,6 @@ export default {
     ...mapState(useAuthStore, ['user']),
     isLoggedIn() {
       return this.user != null;
-    },
-    showUserIcon() {
-      return this.user != null && this.dropdownAvatarName;
     },
     getEmail() {
       return this.user.email;
